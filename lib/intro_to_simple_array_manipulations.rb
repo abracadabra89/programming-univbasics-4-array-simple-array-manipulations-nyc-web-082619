c7529f47d3b86f977b706ec7e11a620594cb31ce
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-10 23:50:14 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,52 @@
+using_push(array, string)
+array = array.push(string)
+end
+
+using_unshift(array, string)
+array = array.unshift(string)
+end
+
+using_pop(arg)
+arg.pop
+end
+
+pop_with_args(args)
+args.pop(2)
+end
+
+using_shift(args)
+args.shift
+end
+
+shift_with_args(array)
+array.shift(2)
+end
+
+using_concat(arg1, arg2)
+arg1.concat(arg2)
+end
+
+using_insert(array,elem)
+array[4].insert(elem)
+end
+
+using_uniq(arg)
+arg.uniq
+end
+
+using_flatten(arg)
+arg.flatten
+end
+
+using_delete(array, string)
+array.delete(string)
+end
+
+using_delete_at(array, integr)
+array.delete(array[integr])
+end
+
+
+
+
+
